03aa5d2bb8a0677d7cf7fcaa062fc8a29cdd5991
Philipp Wilimzig <p.wilimzig@ikusei.de>
2021-02-13 22:13:33 +0100

    Clean Up & new Type ConstructionSite

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,14 +1,14 @@
-import { Roadwork } from "./models/Roadwork.js";
+import { ConstructionSite } from "./models/ConstructionSite.js";
 
 export const resolvers = {
   Query: {
-    roadworks: () => Roadwork.find()
+    constructionSites: () => ConstructionSite.find()
   },
   Mutation: {
-    createRoadwork: async (_, {name, description}) => {
-      const roadwork = new Roadwork({ name, description });
-      await roadwork.save();
-      return roadwork;
+    createConstructionSite: async (_, { category, cause, description, direction, endDate, imageUri, lat, lon, locationDescription, restrictions, startDate, title }) => {
+      const constructionSite = new ConstructionSite({ category, cause, description, direction, endDate, imageUri, location: { lat, lon }, locationDescription, restrictions, startDate, title });
+      await constructionSite.save();
+      return constructionSite;
     }
 
   }
--- src/typeDefs.js
+++ src/typeDefs.js
@@ -2,20 +2,56 @@ import { gql } from "apollo-server-core";
 
 export const typeDefs = gql`
 
-  type Roadwork {
-    id: ID!
-    name: String!
+  "Eine Baustelle"
+  type ConstructionSite {
+    "Art der Baustelle"
+    category: String
+    "Grund der Baustelle"
+    cause: String
+    "Beschreibung der Baustelle"
     description: String
+    "Richtung der Baustelle"
+    direction: String
+    "(Voraussichtliches) Ende der Baustelle"
+    endDate: String
+    "URL einer zugöhrigen Bilddatei"
+    imageUri: String
+    "Geo-Koordinaten der Baustellen"
+    location: Location
+    "Zusätzliche Angabe zum Baustellen-Ort"
+    locationDescription: String
+    "Einschränkungen durch die Baustelle"
+    restrictions:[String]
+    "Start-Datum der Baustelle"
+    startDate: String!
+    "Name/Bezeichner der Baustelle"
+    title: String!
+  }
+
+
+  type Location {
+    lat: String
+    lon: String
   }
 
   type Query {
-    roadworks: [Roadwork!]!
+    constructionSites: [ConstructionSite!]!
   }
 
   type Mutation {
-    createRoadwork(
-      name: String!
+    createConstructionSite(
+      category: String
+      cause: String
       description: String
-    ): Roadwork!
+      direction: String
+      endDate: String
+      imageUri: String
+      lat: String
+      lon: String
+      locationDescription: String
+      restrictions: [String]
+      startDate: String!
+      title: String!
+    ): ConstructionSite!
   }
 `;
