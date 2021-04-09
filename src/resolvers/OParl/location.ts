eb184128ecb6f5e522c24ff764a00e8734ec5ee3
Julian Kwast <jk@tpwd.de>
2021-04-09 17:36:04 +0200

    feat(location): expanded queries

--- src/resolvers/OParl/location.ts
+++ src/resolvers/OParl/location.ts
@@ -5,22 +5,62 @@ import {
   Meeting,
   Organization,
   Paper,
+  Person,
 } from '../../models';
+import { findByIds, getPaginatedEntriesByIds } from '../resolverHelpers';
 
 export const locationResolvers = {
   Query: {
-    oParlLocations: () => Location.find(),
+    oParlLocations: (_, args: { externalIds?: string[] }) =>
+      args.externalIds
+        ? findByIds(args.externalIds, Location)
+        : Location.find(),
   },
   OParlLocation: {
-    bodies: (args: ILocation) =>
-      args.bodies?.map((value) => Body.findOne({ externalId: value })),
-    organization: (args: ILocation) =>
-      args.organization?.map((value) =>
-        Organization.findOne({ externalId: value }),
+    bodies: async (
+      parent: ILocation,
+      args: { offset?: number; pageSize?: number },
+    ) =>
+      getPaginatedEntriesByIds(Body, parent.bodies, args.offset, args.pageSize),
+    organization: async (
+      parent: ILocation,
+      args: { offset?: number; pageSize?: number },
+    ) =>
+      getPaginatedEntriesByIds(
+        Organization,
+        parent.organization,
+        args.offset,
+        args.pageSize,
       ),
-    meeting: (args: ILocation) =>
-      args.meeting?.map((value) => Meeting.findOne({ externalId: value })),
-    papers: (args: ILocation) =>
-      args.papers?.map((value) => Paper.findOne({ externalId: value })),
+    meeting: async (
+      parent: ILocation,
+      args: { offset?: number; pageSize?: number },
+    ) =>
+      getPaginatedEntriesByIds(
+        Meeting,
+        parent.meeting,
+        args.offset,
+        args.pageSize,
+      ),
+    papers: async (
+      parent: ILocation,
+      args: { offset?: number; pageSize?: number },
+    ) =>
+      getPaginatedEntriesByIds(
+        Paper,
+        parent.papers,
+        args.offset,
+        args.pageSize,
+      ),
+    persons: async (
+      parent: ILocation,
+      args: { offset?: number; pageSize?: number },
+    ) =>
+      getPaginatedEntriesByIds(
+        Person,
+        parent.persons,
+        args.offset,
+        args.pageSize,
+      ),
   },
 };
